074f222fbe6659bb549daf34825cbcdc458950d3
Abhishek Goyal <abgoyal@gmail.com>
2025-08-13 10:47:52 +0530

    fix rpm repo cleanup

--- .github/scripts/manage_rpm_repo.sh
+++ .github/scripts/manage_rpm_repo.sh
@@ -41,8 +41,8 @@ for arch_mapping in "amd64:x86_64" "arm64:aarch64"; do
         previous_major_num=$((${current_major} - 1))
         previous_major="${previous_major_num}"
 
-        to_keep_current=$(echo "$versions" | grep "^$current_major" | head -n $KEEP_CURRENT_MAJOR)
-        to_keep_previous=$(echo "$versions" | grep "^$previous_major" | head -n $KEEP_PREVIOUS_MAJOR)
+        to_keep_current=$(echo "$versions" | grep "^$current_major" | head -n $KEEP_CURRENT_MAJOR || true)
+        to_keep_previous=$(echo "$versions" | grep "^$previous_major" | head -n $KEEP_PREVIOUS_MAJOR || true)
         to_keep=$(echo -e "${to_keep_current}\n${to_keep_previous}" | sed '/^\s*$/d' | sort | uniq)
         to_delete=$(comm -23 <(echo "$versions" | sort) <(echo "$to_keep" | sort))
 
